da652a61415f8be47b3a9e9cdab905d3b0407c25
Teezy <Toluwani220@gmail.com>
2025-09-09 10:08:46 +1300

    fix:changed the column back to mapped, mappedcolumn

--- app/model/models.py
+++ app/model/models.py
@@ -1,20 +1,26 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, DateTime, Boolean, Integer
 
-Base = declarative_base()
+
+# Base class for models
+class Base(DeclarativeBase):
+    pass
+
 
 class Contact(Base):
     __tablename__ = "contacts"
 
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    phone_numbers = Column(String, nullable=False)  
-    emails = Column(String, nullable=True) 
-    address = Column(String, nullable=True)
-    birthday = Column(String, nullable=True) 
-    notes = Column(String, nullable=True)
-    favorite = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=False)
+    last_name: Mapped[str] = mapped_column(String, nullable=False)
+    phone_numbers: Mapped[str] = mapped_column(String, nullable=False)
+    emails: Mapped[str | None] = mapped_column(String, nullable=True)
+    address: Mapped[str | None] = mapped_column(String, nullable=True)
+    birthday: Mapped[str | None] = mapped_column(String, nullable=True)
+    notes: Mapped[str | None] = mapped_column(String, nullable=True)
+    favorite: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
